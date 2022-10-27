d45b54210628c4a64cfcabe3e96e599140a89585
maussho <sevdez.94@gmail.com>
2022-10-27 09:19:31 -0400

    go merge usu1

--- archivo-ejemplo.js
+++ archivo-ejemplo.js
@@ -3,17 +3,5 @@ var i = 4*2;
 const nombre = "Mauricio";
 const lastname = "Severiche";
 
-<<<<<<< HEAD
 // New line MAster
 console.log("Hola mundillo")
-=======
-
-// Editando con el usuario 2
-
-const name= "usuario 2";
-const lastname2 = "local";
-console.log(name);
-
-// New line usu1
-console.log("Hello")
->>>>>>> usu1
