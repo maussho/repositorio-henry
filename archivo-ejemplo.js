9178b780634fd9d380bdd500e3d9fb9a1b7f340c
maussho <sevdez.94@gmail.com>
2022-10-27 09:07:31 -0400

    edit first file usu2

--- archivo-ejemplo.js
+++ archivo-ejemplo.js
@@ -2,3 +2,10 @@ var i = 4*2;
 // Colocando comentario Master
 const nombre = "Mauricio";
 const lastname = "Severiche";
+
+
+// Editando con el usuario 2
+
+const name= "usuario 2";
+const lastname2 = "local";
+console.log(name);
